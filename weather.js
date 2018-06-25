04bb239baed8c1c8f5f96b4bf914829686a969d4
matsfisk <greeb@startmail.com>
2018-06-25 17:50:07 +0200

    herlighet, commitet mens jeg jobbet i en annen mappe. commit-kommentarer vedlagt

--- weather.js
+++ weather.js
@@ -2,55 +2,99 @@ $(document).ready(function() {
 
   var oslo = $("#oslo");
   var london = $("#london")
-  var moscow = $("#moscow")
+  var minsk = $("#minsk")
 
-$.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=Oslo,no&APPID=6365d49cbfb4f469b7ff20b44cda9418',
-        type: 'GET',
-        success: function(data){
-          // To get capital letter; beautifying
-          var weather = data.weather[0].description.charAt(0).toUpperCase()  + data.weather[0].description.slice(1);
-          var weatherInfo = $("<div>" + weather + "</div>").appendTo(oslo);
-          weatherInfo.addClass("weather-info")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-            alert(textStatus + errorThrown); //Better diagnostics
-        }
-    });
+  checkWeather(true);
+
+});
 
 
-$.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6365d49cbfb4f469b7ff20b44cda9418',
-        type: 'GET',
-        success: function(data){
-          // To get capital letter; beautifying
-          var weather = data.weather[0].description.charAt(0).toUpperCase()  + data.weather[0].description.slice(1);
-          var weatherInfo = $("<div>" + weather + "</div>").appendTo(london);
-          weatherInfo.addClass("weather-info")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-            alert(textStatus + errorThrown); //Better diagnostics
-        }
-    });
+function checkWeather(firstRun) {
 
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Oslo,no&APPID=6365d49cbfb4f469b7ff20b44cda9418',
+    type: 'GET',
+    success: function(data) {
 
-$.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=6365d49cbfb4f469b7ff20b44cda9418',
-        type: 'GET',
-        success: function(data){
-          // To get capital letter; beautifying
-          var weather = data.weather[0].description.charAt(0).toUpperCase()  + data.weather[0].description.slice(1);
-          var weatherInfo = $("<div>" + weather + "</div>").appendTo(moscow);
-          weatherInfo.addClass("weather-info")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-            alert(textStatus + errorThrown); //Better diagnostics
-        }
-    });
+      // To get capital letter; beautifying
+      var weather = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+
+      var weatherTime = moment().format("dd, MMM Do YY, k:mm");
+
+      if (firstRun) {
+
+        var weatherInfo = $("<div>" + weather + "<br>" + weatherTime + "</div>").appendTo(oslo);
+
+        weatherInfo.attr('id', 'weather-info-oslo')
+        weatherInfo.addClass("weather-info");
+      } else {
+        $('#weather-info-oslo').html("<div>" + weather + "<br>" + weatherTime + "</div>");
+      }
+
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(textStatus + errorThrown); //Better diagnostics
+    }
   });
 
-//
-// $("<div/>", {
-//   class: "user-name",
-//   text: employee.firstname + " " + employee.surname
-// }).appendTo(userBox);
+
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6365d49cbfb4f469b7ff20b44cda9418',
+    type: 'GET',
+    success: function(data) {
+
+      // To get capital letter; beautifying
+      var weather = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+
+      var weatherTime = moment().format("dd, MMM Do YY, k:mm");
+
+      if (firstRun) {
+
+        var weatherInfo = $("<div>" + weather + "<br>" + weatherTime + "</div>").appendTo(london);
+
+        weatherInfo.attr('id', 'weather-info-london')
+        weatherInfo.addClass("weather-info");
+
+      } else {
+
+        $('#weather-info-london').html("<div>" + weather + "<br>" + weatherTime + "</div>");
+
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(textStatus + errorThrown); //Better diagnostics
+    }
+  });
+
+
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Minsk,by&APPID=6365d49cbfb4f469b7ff20b44cda9418',
+    type: 'GET',
+    success: function(data) {
+
+      // To get capital letter; beautifying
+      var weather = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+      
+      var weatherTime = moment().format("dd, MMM Do YY, k:mm");
+
+      if (firstRun) {
+        var weatherInfo = $("<div>" + weather + "<br>" + weatherTime + "</div>").appendTo(minsk);
+
+        weatherInfo.attr('id', 'weather-info-minsk')
+        weatherInfo.addClass("weather-info");
+
+      } else {
+
+        $('#weather-info-minsk').html("<div>" + weather + "<br>" + weatherTime + "</div>");
+
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(textStatus + errorThrown); //Better diagnostics
+    }
+  });
+  setTimeout(function() {
+    checkWeather(false);
+  }, 60000);
+
+};
